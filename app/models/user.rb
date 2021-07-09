9bfc220ff2862af16cff08ebb228056780acd839
Vishak Amin <aminvishak@gmail.com>
2021-07-10 00:39:50 +0530

    Added has_secure_password and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,24 @@
 class User < ApplicationRecord  
-  validates :first_name, presence: true, length: { maximum: 35 }
-  has_many :polls, dependent: :destroy
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
 
+  validates :first_name, presence: true, length: { maximum: 35 }
+  has_many :polls, dependent: :destroy, foreign_key: :user_id
+  has_secure_password
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, on: :create
+
+  before_save :to_lowercase
+
+  private
+
+  def to_lowercase
+    email.downcase!
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_182047) do
+ActiveRecord::Schema.define(version: 2021_07_09_185640) do
 
   create_table "polls", force: :cascade do |t|
     t.text "title", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2021_07_09_182047) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "polls", "users", on_delete: :cascade
